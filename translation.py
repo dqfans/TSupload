7a5a8fa64ab89369f62d94cb118a666824f36722
noupus <63485872+noupus@users.noreply.github.com>
2020-04-18 20:58:13 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,8 +1,7 @@
 class Translation(object):
     START_TEXT = """Hi Ⓓⓤⓓⓔ, Thank You for using me.
 /help to know how to use me. Please Send a direct (video) link, and I will try to upload on Telegram.
-© @TSuploadbot
-Subscribe @TS_Projects if you 💔 using this bot..."""
+© @TSuploadbot"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = ""
