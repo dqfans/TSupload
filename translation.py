7e9eb8bcfbd9528ce8485e32a12a0166ef842e10
noupus <63485872+noupus@users.noreply.github.com>
2020-06-02 10:20:02 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,11 +1,7 @@
 class Translation(object):
     START_TEXT = """𝐇𝐢, 𝐓𝐡𝐚𝐧𝐤 𝐘𝐨𝐮 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 𝐦𝐞.
 /help 𝐭𝐨 𝐤𝐧𝐨𝐰 𝐡𝐨𝐰 𝐭𝐨 𝐮𝐬𝐞 𝐦𝐞. 𝐏𝐥𝐞𝐚𝐬𝐞 𝐒𝐞𝐧𝐝 𝐚 𝐝𝐢𝐫𝐞𝐜𝐭 (𝐯𝐢𝐝𝐞𝐨) 𝐥𝐢𝐧𝐤, 𝐚𝐧𝐝 𝐈 𝐰𝐢𝐥𝐥 𝐭𝐫𝐲 𝐭𝐨 𝐮𝐩𝐥𝐨𝐚𝐝 𝐨𝐧 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦.​
-© @TSuploadbot 😍 
-                [
-                    InlineKeyboardButton('Help', url='https://telegra.ph/Features-of-TS-ScreenshotBot-05-31'),
-                    InlineKeyboardButton('Project Channel', url='https://t.me/TSproject')
-                ]"""
+© @TSuploadbot 😍 """
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = ""
