29fcdbef69ecb873a8431c53759b1a7f355968cd
noupus <63485872+noupus@users.noreply.github.com>
2020-06-06 08:50:12 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,14 +1,14 @@
 class Translation(object):
     START_TEXT = """𝐇𝐢, 𝐓𝐡𝐚𝐧𝐤 𝐘𝐨𝐮 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 𝐦𝐞.
 /help 𝐭𝐨 𝐤𝐧𝐨𝐰 𝐡𝐨𝐰 𝐭𝐨 𝐮𝐬𝐞 𝐦𝐞. 𝐏𝐥𝐞𝐚𝐬𝐞 𝐒𝐞𝐧𝐝 𝐚 𝐝𝐢𝐫𝐞𝐜𝐭 (𝐯𝐢𝐝𝐞𝐨) 𝐥𝐢𝐧𝐤, 𝐚𝐧𝐝 𝐈 𝐰𝐢𝐥𝐥 𝐭𝐫𝐲 𝐭𝐨 𝐮𝐩𝐥𝐨𝐚𝐝 𝐨𝐧 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦.​
-© @TSuploadbot 😍 
+© @TSuploadbot 😍 """
     reply_markup=InlineKeyboardMarkup(
             [
                 
                 [InlineKeyboardButton('My Boss', url='https://t.me/TS_Boss')],
             ]
         )
-    )"""
+    )
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = ""
